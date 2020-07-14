561cdc6d92bcedac10da767b5d35351111998112
Uri Goren <uri@goren.ml>
2020-07-14 08:37:38 +0300

    decode on save

--- save.php
+++ save.php
@@ -4,7 +4,7 @@ if (!array_key_exists("id", $_POST)||!is_numeric($_POST["id"]))
 $id = $_POST["id"];
 $data = file_get_contents("annotations/$id.json");
 $data = json_decode($data, TRUE);
-$data['labels']=$_POST['events'];
+$data['labels']=json_decode($_POST['events']);
 $data['status']=1;
 $data = json_encode($data);
 file_put_contents("annotations/$id.json", $data);
